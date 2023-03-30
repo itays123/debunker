7ad353bb00b41631de767756090988672fe2ac9f
itays123 <itays2005@gmail.com>
2023-03-30 12:27:31 +0300

    INIT: Home page structure

---
+++ debunker-website/tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        open: ['"Open Sans"', ...fontFamily.sans], // make Open Sans the default font
+      },
+    },
+  },
+  plugins: [],
+};
